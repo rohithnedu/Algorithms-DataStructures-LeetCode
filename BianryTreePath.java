c4987c63e2137597d63fcde9c6ddd449584f7d54
Rohith Nedunuri <nedunurirohith@gmail.com>
2017-05-22 04:34:49 -0400

    BinaryTreeRootToPath

--- BianryTreePath.java
+++ BianryTreePath.java
@@ -60,14 +60,16 @@ public List<String> binaryTreePaths(TreeNode root) {
     Stack<TreeNode> sNode=new Stack<TreeNode>();
     Stack<String> sStr=new Stack<String>();
     
-    if(root==null) return list;
+    if(root==null) 
+		return list;
     sNode.push(root);
     sStr.push("");
     while(!sNode.isEmpty()) {
         TreeNode curNode=sNode.pop();
         String curStr=sStr.pop();
         
-        if(curNode.left==null && curNode.right==null) list.add(curStr+curNode.val);
+        if(curNode.left==null && curNode.right==null)
+			list.add(curStr+curNode.val);
         if(curNode.left!=null) {
             sNode.push(curNode.left);
             sStr.push(curStr+curNode.val+"->");
